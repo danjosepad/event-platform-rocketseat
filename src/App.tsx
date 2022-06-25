e9466215897a289c8c8c6bc5247e48b5d761c0dd
danjosepad <danielpadilhapessoal@outlook.com>
2022-06-25 18:10:02 -0300

    chore: add query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
+import { ApolloProvider, gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./service/apollo"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+    id
+    slug
+    title
+    teacher {
+      id
+      name
+      bio
+      avatarURL
+    }
+    lessonType
+    description
+    }
+  }
+`
+
+interface Lesson {
+  id: string
+  title: string
+}
+
 function App() {
+  const { data, loading } = useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY)
+
+  if (loading) {
+    return <div>loading...</div>
+  }
 
   return (
-    <div className="text-2xl">
-      Hello world
-    </div>
+    <ul className="text-2xl">
+      {data?.lessons.map((lesson: Lesson) => (
+        <li key={lesson.id}>{lesson.title}</li>
+      ))}
+    </ul>
   )
 }
 
