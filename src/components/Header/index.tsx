811530aa638b34b923387862668fd07d31af528a
danjosepad <danielpadilhapessoal@outlook.com>
2022-06-26 11:25:52 -0300

    chore: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { Icons } from "../../assets/Icons"
+
+export const Header = () => {
+  return (
+    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+      <Icons.Logo />
+    </header>
+  )
+}
