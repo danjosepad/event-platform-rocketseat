3268d32dd1ffdb555163299943bb64033d8ad0d2
danjosepad <danielpadilhapessoal@outlook.com>
2022-06-26 11:27:41 -0300

    feat: add event page on main

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,8 @@
-import { ApolloProvider, gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./service/apollo"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-    id
-    slug
-    title
-    teacher {
-      id
-      name
-      bio
-      avatarURL
-    }
-    lessonType
-    description
-    }
-  }
-`
-
-interface Lesson {
-  id: string
-  title: string
-}
+import Event from "./pages/Event"
 
 function App() {
-  const { data, loading } = useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY)
-
-  if (loading) {
-    return <div>loading...</div>
-  }
-
   return (
-    <ul className="text-2xl">
-      {data?.lessons.map((lesson: Lesson) => (
-        <li key={lesson.id}>{lesson.title}</li>
-      ))}
-    </ul>
+    <Event />
   )
 }
 
